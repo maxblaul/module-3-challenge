756e22abe23bd99056ceab31f40ce88b746bd89e
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-09-21 01:54:55 -0500

    made changes to homework

--- script.js
+++ script.js
@@ -5,24 +5,28 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword () {
   console.log ("generate passowrd function running")
   var passwordLength;
-//open ended questions for users
+// open ended questions for users
 // yes o rno questions for users
-
+function letpasswordLength = prompt("Pick a number between 8 and 128 for your password length.")
+   console.log
 //if (its invalid **multiple conditions in if statement**) {return "You must enter a valid integer"}
-  var passwordBase;  //string or array, up to you (howver you store the characters)
-  var lowercaseLetters; //if you go with string or array
-  var userWantsLowerCase;
-  var userWantsUpperCase;
-  var userWantsNumbers;
-  var userWantsSpecialCharacters;
+  var lowercaseLetters =  ["abcdefghijklmnopqrstuvwxyz"]
+  var upperCaseLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+  var numericCharacters = ["1234567890"]
+  var specialCharacters = ["~`!@#$%^&*()_-><.,;:'?+={[*/}]"]
   var selectOne = false
 
+// let passwordLength = prompt("Enter the length of the password you want, between 8 and 128 characters.")
+// passwordLength = par(passwordLength)
+
+if (isNaN(passwordLength)) passwordLength < 8 || passwordLength > 128
+  alert("Enter a number between 8 and 128")
 
   if (selectOne == false){
     console.log ("it wasn't selected")
     return "You Must Select One Option"
   }
-
+  
   var password = "Max Blaul"
   return password 
 }
