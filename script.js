f3db617b6619fa6f2ecde7504426d5e127b0b7a9
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-09-21 10:05:13 -0500

    homework 3 changes

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 // Assignment Code
+var lowercaseLetters =  ["abcdefghijklmnopqrstuvwxyz"]
+var upperCaseLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var numericCharacters = ["1234567890"]
+var specialCharacters = ["~`!@#$%^&*()_-><.,;:'?+={[*/}]"]
 var generateBtn = document.querySelector("#generate");
 
+
+function getCriteria(){
+  var passwordLength = parseInt(prompt("How many characters would you like your password to contain?"));
+  console.log(passwordLength);
+  if(Number.isNaN(passwordLength)){
+    alert("Password length must be a number.")
+    return null;
+  }
+  if(passwordLength < 8 || passwordLength > 128){
+    alert("Password length must be atleast 8 chars and no more than 128");
+    return null;
+  }
+  // if(passwordLength > 128){
+  //   alert("Password length must be less than 128 chars");
+  //   return null;
+  // }
+  var hasNumbers = confirm("Click OK to confirm using numeric chars");
+  var hasUpper = confirm("Click OK to confirm using upper chars");
+  var hasLower = confirm("Click Ok to confirm using lower chars");
+  var hasSpecial = confirm("Click OK to confirm using special chars");
+
+  if(!hasNumbers &&
+     !hasUpper &&
+     !hasLower &&
+     !hasSpecial){
+      alert("Must select atleast on char type");
+      return null;
+     }
+
+     var passwordCriteria = {
+      passwordLength: passwordLength,
+      hasSpecial: hasSpecial,
+      hasUpper: hasUpper,
+      hasLower: hasLower,
+      hasNumbers: hasNumbers
+     };
+
+     return passwordCriteria;
+}
 // Write password to the #password input
 function generatePassword () {
   console.log ("generate passowrd function running")
-  var passwordLength;
-// open ended questions for users
-// yes o rno questions for users
-function letpasswordLength = prompt("Pick a number between 8 and 128 for your password length.")
-   console.log
-//if (its invalid **multiple conditions in if statement**) {return "You must enter a valid integer"}
-  var lowercaseLetters =  ["abcdefghijklmnopqrstuvwxyz"]
-  var upperCaseLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-  var numericCharacters = ["1234567890"]
-  var specialCharacters = ["~`!@#$%^&*()_-><.,;:'?+={[*/}]"]
-  var selectOne = false
-
-// let passwordLength = prompt("Enter the length of the password you want, between 8 and 128 characters.")
-// passwordLength = par(passwordLength)
-
-if (isNaN(passwordLength)) passwordLength < 8 || passwordLength > 128
-  alert("Enter a number between 8 and 128")
-
-  if (selectOne == false){
-    console.log ("it wasn't selected")
-    return "You Must Select One Option"
-  }
-  
-  var password = "Max Blaul"
-  return password 
+  var criteria = getCriteria();
+  console.log(criteria);
+  var password = "Max Blaul";
+  return password ;
 }
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -41,7 +64,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var firstName = "Al"
-var firstNameHalf = "ex"
-firstName = firstName.concat(firstNameHalf)
-console.log(firstName)
+// var firstName = "Al"
+// var firstNameHalf = "ex"
+// firstName = firstName.concat(firstNameHalf)
+// console.log(firstName)
